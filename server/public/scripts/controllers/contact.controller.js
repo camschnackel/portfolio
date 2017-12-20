ea6ac06642886d1c62b4da4609c5c04fdab1b6d0
Cam <cam.schnackel@gmail.com>
2017-12-20 15:19:18 -0600

    fully implemented nodemailer

--- server/public/scripts/controllers/contact.controller.js
+++ server/public/scripts/controllers/contact.controller.js
@@ -1,6 +1,29 @@
-myApp.controller('ContactController', function ($scope) {
+myApp.controller('ContactController', function ($scope, ContactService) {
     console.log('ContactController created');
-    var vm = this;
-
     $scope.currNav = 'contact';
+
+    var vm = this;
+    vm.contactForm = {
+        name: this.name,
+        email: this.email,
+        message: this.message
+    }
+    vm.modal = false;
+    vm.modalToggle = function () {
+        vm.modal = !vm.modal;
+    }
+
+    vm.clearFields = function () {
+        vm.contactForm.name = '';
+        vm.contactForm.email = '';
+        vm.contactForm.message = '';
+    }
+
+    vm.sendMessage = function () {
+        console.log('sendMessage ran');
+        ContactService.sendMessage(vm.contactForm).then( function() {
+            vm.modalToggle();
+            vm.clearFields();
+        });
+    }
 });
