10d4e4578c7524cd34b51973cd972d3498ea3011
Cam <cam.schnackel@gmail.com>
2017-12-20 12:42:42 -0600

    implemented modal for project app links along with more info parameters

--- server/public/scripts/controllers/projects.controller.js
+++ server/public/scripts/controllers/projects.controller.js
@@ -10,6 +10,7 @@ myApp.controller('ProjectsController', function ($scope, GithubService) {
         {
             name: 'FreshList',
             url: 'https://solo-fresh-list.herokuapp.com/',
+            info: 'Please note that it may take up to 30 seconds to appear on intial load due to hosting limitations.',
             tech: 'MEAN Stack (MongoDB, Express.js, AngularJS, Node.js), HTML5, CSS3, JavaScript, Angular Material, Passport, Edamom Recipe API',
             images: ['../../images/screenFresh1.png', '../../images/screenFresh2.png'],
             description: 'FreshList is a full-stack and mobile responsive application that was created to rememdy a personal bad habit I had of wasting groceries and not knowing what to cook.It allows the user to intuitively enter and track their grocery items which are sorted by their expiry date.Beside each grocery item in the list, there is a button that when clicked will provide the user with up to 5 recipe suggestions generated through the Edamom Recipe API.'
@@ -17,6 +18,9 @@ myApp.controller('ProjectsController', function ($scope, GithubService) {
 
         {
             name: 'GrowNorth',
+            url: 'http://grownorthdemo.herokuapp.com/',
+            info: 'Please note that it may take up to 30 seconds to appear on intial load due to hosting limitations.',
+            creds: 'Username: "guest" Password: "guest"',
             tech: 'pSQL, Express.js, AngularJS, Node.js, HTML5, CSS3, JavaScript, Angular Material, AWS, Nodemailer, Passport',
             images: ['../../images/screenGrow1.png', '../../images/screenGrow2.png'],
             description: 'This app was created for a client working for a startup known as Grow North. She had been tracking local agriculture volunteers and entrepreneurs using a rather inefficient process based partially from memory. Together with five other developers, we created a full stack application that improved her engagement with her clients by encouraging participation in her project via survey forms that made the data easily searchable and organized. My role within the project was varied, but I was most involved in the public facing survey forms which I ensured were user friendly and intuitive.'
@@ -36,7 +40,12 @@ myApp.controller('ProjectsController', function ($scope, GithubService) {
             description: 'Everlist was my first crack at a basic to-do list app. Using Bootstrap and jQuery, it allows the user to enter a task with an optional field for a due date. Tasks can be checked off and deleted, with completed tasks sent to the bottom of the page. '
         }
     ]
-    vm.format = 'MMM/d/yy'
+    vm.format = 'MMM/d/yy';
+    vm.modal = {};
+    vm.modalToggle = function(index){
+        console.log('vm.modalToggle ran!');
+        vm.modal[index] = !vm.modal[index];
+    }
 
     console.log('vm.user ->', vm.user);
     console.log('vm.repos ->', vm.repos);
