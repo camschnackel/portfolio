fa7c150cae700ad5baf29c399b2080b28e1baa08
Cam <cam.schnackel@gmail.com>
2022-12-01 00:05:30 -0600

    added Blue Ocean project and implemented link

--- server/public/scripts/controllers/projects.controller.js
+++ server/public/scripts/controllers/projects.controller.js
@@ -12,6 +12,13 @@ myApp.controller('ProjectsController', function ($scope, GithubService) {
             tech: 'React.js, JavaScript, Typescript, MaterialUI, Freemarker, Liferay, Gradle, npm, and Java',
             images: ['../../images/screenIHR1.png', '../../images/screenIHR2.png'],
             description: 'The Individual Health Record™, or IHR, is a technology platform owned and operated by UnitedHealth Group that provides a digital record of a person\'s health care history. It provides and all-in-one record of an individual\'s health, enabling a person and their care team to improve collaboration and care. My task during this project was to aid in a complete front end redesign of the portal. Together alongside a team of talented developers, we refined the code strcture into clean reusable components, dramatically improved performance, added mobile support, implemented support for screen readers, and brought the site into the modern era in terms of design and polish.'
+        },
+                {
+            name: 'Blue Ocean Properties',
+            tech: 'Angular 15, Typescript, PrimeNG / PrimeFlex, HTML5, Sass, and npm',
+            images: ['../../images/screenBlue1.png', '../../images/screenBlue2.png'],
+            description: 'Blue Ocean Properties had a highly outdated Wix site that needed an update that would better showcase their talent in home renovations. This pro-bono project gave me the opportunity to learn Angular 15, and was created as a straightforward means for potential clients to see some of the skilled work Blue Ocean has done throughout their myriad of projects. It features an eye-catching \"splash screen\" of sorts, a gallery of project photos, and a card containing contact information.',
+            url: 'http://www.blueoceanproperties.co'
         }
     ];
     vm.miscRepos = [
